e59827a340d9399c1de5ce77f244c30da95f4617
linfengli <linfengli.larry@icloud.com>
2021-08-19 17:45:25 +0800

    Complete North Flow

--- stock_trading/north_flow/north_flow_engine.py
+++ stock_trading/north_flow/north_flow_engine.py
@@ -1,7 +1,10 @@
 import json
+from typing import Tuple, List
 from datetime import datetime, timedelta
 import pandas as pd
 import tushare as ts
+import plotly
+import plotly.graph_objs as go
 
 
 class NorthFlowEngine(object):
@@ -42,27 +45,68 @@ class NorthFlowEngine(object):
 
         return trade_dates
 
-    def get_hsgt_stocks(self, trade_date):
+    def get_hsgt_stocks(self, trade_date: datetime) -> Tuple:
+        """"""
+        str_dt = pd.to_datetime(trade_date).strftime("%Y%m%d")
+        h_stocks = self.pro.hsgt_top10(trade_date=str_dt, market_type="1")
+        s_stocks = self.pro.hsgt_top10(trade_date=str_dt, market_type="3")
+        # hs_stocks = self.pro.hsgt_top10(trade_date=trade_date)
+
+        return h_stocks, s_stocks
+
+    @staticmethod
+    def north_flow_plot(
+            trade_dates: List, h_series: pd.Series, s_series: pd.Series) -> None:
         """"""
 
-        # h_stocks = self.pro.hsgt_top10(trade_date=trade_date, market_type="1")
-        # s_stocks = self.pro.hsgt_top10(trade_date=trade_date, market_type="3")
-        # hs_stocks = pd.concat([h_stocks, s_stocks], ignore_index=True)
+        start_trade = trade_dates[0]
+        end_trade = trade_dates[-1]
+        trade_length = len(trade_dates)
+        h_trace = go.Bar(
+            x=h_series.index, y=h_series.values,
+            name="Shanghai"
+        )
+        s_trace = go.Bar(
+            x=s_series.index, y=s_series.values,
+            name="Shenzhen"
+        )
+        layout = go.Layout(
+            legend={"x": 1, "y": 1},
+            title=f"North Flow: {start_trade} - {end_trade} ({trade_length} trading days)"
+        )
+        fig = go.Figure(data=[h_trace, s_trace], layout=layout)
 
-        hs_stocks = self.pro.hsgt_top10(trade_date=trade_date)
+        plotly.offline.plot(fig, filename="north_flow.html")
 
-        return hs_stocks
-
-    def north_flow_main(self):
+    def north_flow_main(self) -> Tuple:
         """"""
 
         trade_dates = self.get_trade_dates()
 
+        dts = []
+        h_data, s_data = pd.DataFrame(), pd.DataFrame()
         for trade_date in trade_dates:
-            hs_df = self.get_hsgt_stocks(trade_date=trade_date)
+            h_df, s_df = self.get_hsgt_stocks(trade_date=trade_date)
+            if h_df.empty or s_df.empty:
+                continue
+            dts.append(pd.to_datetime(trade_date).strftime("%Y-%m-%d"))
+            h_data = pd.concat([h_data, h_df], ignore_index=True)
+            s_data = pd.concat([s_data, s_df], ignore_index=True)
+
+        h_series = h_data.groupby("name")["net_amount"].sum().sort_values(ascending=False)
+        s_series = s_data.groupby("name")["net_amount"].sum().sort_values(ascending=False)
+
+        self.north_flow_plot(dts, h_series, s_series)
+
+        return h_series, s_series
 
 
 if __name__ == "__main__":
+    """"""
 
-    pass
+    e_date = datetime.now()
+    s_date = e_date - timedelta(days=5)
+    engine = NorthFlowEngine(s_date, e_date)
+
+    sh_series, sz_series = engine.north_flow_main()
 
